7c3455d2c5678812f1fe02ea5d6b046341734253
Zander Work <zander@zanderwork.com>
2021-04-19 01:43:20 -0700

    added config options for new/removed victim notifications (#3)

--- src/notifications/manager.py
+++ src/notifications/manager.py
@@ -7,13 +7,17 @@ from .slack import SlackNotification
 class NotificationManager():
     def send_new_victim_notification(victim: Victim):
         for workspace, params in Config["slack"].items():
-            # deliberately don't break out of the loop if one fails, continue trying the others
+            if not params["new_victims"]:
+                continue
+
             if not SlackNotification.send_new_victim_notification(params["url"], victim):
                 logging.error(f"Failed to send new victim notification to Slack workspace \"{workspace}\"")
     
     def send_victim_removed_notification(victim: Victim):
         for workspace, params in Config["slack"].items():
-            # deliberately don't break out of the loop if one fails, continue trying the others
+            if not params["removed_victims"]:
+                continue
+
             if not SlackNotification.send_victim_removed_notification(params["url"], victim):
                 logging.error(f"Failed to send removed victim notification to Slack workspace \"{workspace}\"")
     
@@ -22,7 +26,6 @@ class NotificationManager():
             if not params["down_sites"]:
                 continue
 
-            # deliberately don't break out of the loop if one fails, continue trying the others
             if not SlackNotification.send_site_down_notification(params["url"], site):
                 logging.error(f"Failed to send site down notification to Slack workspace \"{workspace}\"")
     
@@ -31,6 +34,5 @@ class NotificationManager():
             if not params["errors"]:
                 continue
 
-            # deliberately don't break out of the loop if one fails, continue trying the others
             if not SlackNotification.send_error_notification(params["url"], context, error, fatal):
                 logging.error(f"Failed to send error notification to Slack workspace \"{workspace}\"")
