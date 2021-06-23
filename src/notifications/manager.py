cb0a4ffa8707988c135cca85dae5d456b027ef57
Bili Gonzales <86288273+biligonzales@users.noreply.github.com>
2021-06-23 07:16:49 +0200

    Handle missing notifications element in the yaml config file (#52) (#53)

--- src/notifications/manager.py
+++ src/notifications/manager.py
@@ -7,57 +7,61 @@ from .discord import DiscordNotification
 
 class NotificationManager():
     def send_new_victim_notification(victim: Victim):
-        for dest, params in Config["notifications"].items():
-            if not params["new_victims"]:
-                continue
+        if "notifications" in Config and Config["notifications"]:
+            for dest, params in Config["notifications"].items():
+                if not params["new_victims"]:
+                    continue
 
-            if params["type"] == "slack":
-                if not SlackNotification.send_new_victim_notification(params["url"], victim):
-                    logging.error(f"Failed to send new victim notification to Slack workspace \"{dest}\"")
-            elif params["type"] == "discord":
-                if not DiscordNotification.send_new_victim_notification(params["url"], victim):
-                    logging.error(f"Failed to send new victim notification to Discord guild \"{dest}\"")
-            else:
-                logging.error(f"Attempted to send a new victim notification to an unsupported notification type: {params['type']}")
+                if params["type"] == "slack":
+                    if not SlackNotification.send_new_victim_notification(params["url"], victim):
+                        logging.error(f"Failed to send new victim notification to Slack workspace \"{dest}\"")
+                elif params["type"] == "discord":
+                    if not DiscordNotification.send_new_victim_notification(params["url"], victim):
+                        logging.error(f"Failed to send new victim notification to Discord guild \"{dest}\"")
+                else:
+                    logging.error(f"Attempted to send a new victim notification to an unsupported notification type: {params['type']}")
     
     def send_victim_removed_notification(victim: Victim):
-        for dest, params in Config["notifications"].items():
-            if not params["removed_victims"]:
-                continue
+        if "notifications" in Config and Config["notifications"]:
+            for dest, params in Config["notifications"].items():
+                if not params["removed_victims"]:
+                    continue
 
-            if params["type"] == "slack":
-                if not SlackNotification.send_victim_removed_notification(params["url"], victim):
-                    logging.error(f"Failed to send removed victim notification to Slack workspace \"{dest}\"")
-            elif params["type"] == "discord":
-                if not DiscordNotification.send_victim_removed_notification(params["url"], victim):
-                    logging.error(f"Failed to send removed victim notification to Discord guild \"{dest}\"")
-            else:
-                logging.error(f"Attempted to send a removed victim notification to an unsupported notification type: {params['type']}")
+                if params["type"] == "slack":
+                    if not SlackNotification.send_victim_removed_notification(params["url"], victim):
+                        logging.error(f"Failed to send removed victim notification to Slack workspace \"{dest}\"")
+                elif params["type"] == "discord":
+                    if not DiscordNotification.send_victim_removed_notification(params["url"], victim):
+                        logging.error(f"Failed to send removed victim notification to Discord guild \"{dest}\"")
+                else:
+                    logging.error(f"Attempted to send a removed victim notification to an unsupported notification type: {params['type']}")
     
     def send_site_down_notification(site: Site):
-        for dest, params in Config["notifications"].items():
-            if not params["down_sites"]:
-                continue
+        if "notifications" in Config and Config["notifications"]:
+            for dest, params in Config["notifications"].items():
+                if not params["down_sites"]:
+                    continue
 
-            if params["type"] == "slack":
-                if not SlackNotification.send_site_down_notification(params["url"], site):
-                    logging.error(f"Failed to send site down notification to Slack workspace \"{dest}\"")
-            elif params["type"] == "discord":
-                if not DiscordNotification.send_site_down_notification(params["url"], site):
-                    logging.error(f"Failed to send site down notification to Discord guild \"{dest}\"")
-            else:
-                logging.error(f"Attempted to send a site down notification to an unsupported notification type: {params['type']}")
+                if params["type"] == "slack":
+                    if not SlackNotification.send_site_down_notification(params["url"], site):
+                        logging.error(f"Failed to send site down notification to Slack workspace \"{dest}\"")
+                elif params["type"] == "discord":
+                    if not DiscordNotification.send_site_down_notification(params["url"], site):
+                        logging.error(f"Failed to send site down notification to Discord guild \"{dest}\"")
+                else:
+                    logging.error(f"Attempted to send a site down notification to an unsupported notification type: {params['type']}")
     
     def send_error_notification(context: str, error: str, fatal: bool = False):
-        for dest, params in Config["notifications"].items():
-            if not params["errors"]:
-                continue
+        if "notifications" in Config and Config["notifications"]:
+            for dest, params in Config["notifications"].items():
+                if not params["errors"]:
+                    continue
 
-            if params["type"] == "slack":
-                if not SlackNotification.send_error_notification(params["url"], context, error, fatal):
-                    logging.error(f"Failed to send error notification to Slack workspace \"{dest}\"")
-            elif params["type"] == "discord":
-                if not DiscordNotification.send_error_notification(params["url"], context, error, fatal):
-                    logging.error(f"Failed to send error notification to Discord guild \"{dest}\"")
-            else:
-                logging.error(f"Attempted to send a site down notification to an unsupported notification type: {params['type']}")
+                if params["type"] == "slack":
+                    if not SlackNotification.send_error_notification(params["url"], context, error, fatal):
+                        logging.error(f"Failed to send error notification to Slack workspace \"{dest}\"")
+                elif params["type"] == "discord":
+                    if not DiscordNotification.send_error_notification(params["url"], context, error, fatal):
+                        logging.error(f"Failed to send error notification to Discord guild \"{dest}\"")
+                else:
+                    logging.error(f"Attempted to send a site down notification to an unsupported notification type: {params['type']}")
