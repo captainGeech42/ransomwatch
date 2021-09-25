b0e486cca8697b2aea4d271e9bd2799ea974e18c
X-Originating-IP <tomrsamson@gmail.com>
2021-09-25 16:19:00 +0100

    Create lv.py
    Added support for LV

---
+++ src/sites/lv.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import logging
+
+from bs4 import BeautifulSoup
+
+from db.models import Victim
+from net.proxy import Proxy
+from .sitecrawler import SiteCrawler
+
+
+class LV(SiteCrawler):
+    actor = "LV"
+
+    def scrape_victims(self):
+        with Proxy() as p:
+            r = p.get(f"{self.url}", headers=self.headers)
+
+            soup = BeautifulSoup(r.content.decode(), "html.parser")
+
+            # get max page number
+            victim_list = soup.find_all("div", class_="blog-post blog-main posts_at_first")
+
+            for victim in victim_list:
+                print(victim)
+                victim_name = victim.find("h2", class_="blog-post-title").find("a").text.strip()
+
+                published = ""
+
+                victim_leak_site = self.url + victim.find("h2", class_="blog-post-title").find("a").attr["href"]
+
+                q = self.session.query(Victim).filter_by(
+                    url=victim_leak_site, site=self.site)
+
+                if q.count() == 0:
+                    # new victim
+                    v = Victim(name=victim_name, url=victim_leak_site, published=published_dt,
+                               first_seen=datetime.utcnow(), last_seen=datetime.utcnow(), site=self.site)
+                    self.session.add(v)
+                    self.new_victims.append(v)
+                else:
+                    # already seen, update last_seen
+                    v = q.first()
+                    v.last_seen = datetime.utcnow()
+
+                # add the org to our seen list
+                self.current_victims.append(v)
+            self.session.commit()
+
+        self.site.last_scraped = datetime.utcnow()
+
+        # just for good measure
+        self.session.commit()
