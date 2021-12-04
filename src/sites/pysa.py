dd539989efa85297fc4cb7167aed34401c019fe2
Zander Work <zander@zanderwork.com>
2021-12-04 10:43:10 -0500

    fixed #77, changed dateparsing to use lib (#82)
    closes #77

--- src/sites/pysa.py
+++ src/sites/pysa.py
@@ -1,13 +1,13 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import logging
+from datetime import datetime
 
 from bs4 import BeautifulSoup
+import dateparser
+from dateutil.relativedelta import relativedelta
 
 from db.models import Victim
 from net.proxy import Proxy
 from .sitecrawler import SiteCrawler
-
 
 class Pysa(SiteCrawler):
     actor = "Pysa"
@@ -21,34 +21,17 @@ class Pysa(SiteCrawler):
 
             # get max page number
             victim_list = soup.find_all("div", class_="page-header")
-            last_published_dt = None
 
             for victim in victim_list:
                 victim_name = victim.find_all("a")[0].text.strip()
                 published = victim.find_all("span")[1].text.strip()
 
-                # they use a bunch of different date format...
-                published_dt = None
-                year_format = "%y"
-                if len(published.split('/')[2]) == 4:
-                    year_format = "%Y"
-                try:
-                    published_dt = datetime.strptime(
-                        published, "%m/%d/{}".format(year_format))
-                    # as we don't know the format of the date, let's check if the month detected
-                    # is near the previous considered month
-                    if last_published_dt and ((published_dt.month != last_published_dt.month)
-                        or (published_dt.month != (last_published_dt + relativedelta(months=-1)).month)):
-                        published_dt = datetime.strptime(
-                            published, "%d/%m/{}".format(year_format))
-                except ValueError as ve:
-                    try:
-                        published_dt = datetime.strptime(
-                            published, "%d/%m/{}".format(year_format))
-                    except ValueError as ve:
-                        logging.error("Unable to cast date {}".format(published))
-
-                last_published_dt = published_dt
+                # they use a bunch of different date format
+                # use a nice dateparsing library to handle them all in an easier manner
+                published_dt = dateparser.parse(published)
+                # sometimes they don't have a timestamp
+                if published_dt is None and len(published) > 0:
+                    logging.warning(f"couldn't parse timestamp: {published}")
 
                 victim_leak_site = self.url + '/' + victim.find_all("a")[0].attrs["href"]
                 q = self.session.query(Victim).filter_by(
